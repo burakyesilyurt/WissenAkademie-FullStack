a9492f3b32f31f31267573f8cb32edcac5fdfa65
Burak Yesilyurt <yesilyurt.burak07@gmail.com>
2024-07-24 19:02:19 +0300

    added ViewModelApplication

---
+++ ViewModelApplication/Models/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewModelApplication.Models
+{
+    public class Gender
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
